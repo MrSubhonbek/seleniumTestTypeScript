a1a1c18cb7c50521c22e30685346e6efc45324de
Subhonbek <basubhonbek@gmail.com>
2022-06-30 13:32:57 +0300

    add test

--- src/Components/AddTodo.tsx
+++ src/Components/AddTodo.tsx
@@ -1,5 +1,6 @@
 import { AddTodoProps } from "../types"
 import { ReactComponent as PlusIcon } from "../assets/svg/plus.svg"
+import { v4 as uuidv4 } from "uuid"
 
 export const AddTodo = ({
   handleSubmitTodo,
@@ -9,6 +10,7 @@ export const AddTodo = ({
   <form className="flex justify-between w-full" onSubmit={handleSubmitTodo}>
     <input
       type="text"
+      id="inputId"
       name="task"
       value={task}
       className="flex-1 rounded shadow p-2 text-grey-dark mr-2"
--- tests/test.ts
+++ tests/test.ts
@@ -1,11 +1,21 @@
-const {Builder, By, Key}  = require ('selenium-webdriver');
-
-async function example () {
+const { Builder, By, Key } = require('selenium-webdriver');
+const assert = require('assert')
+async function example() {
+    //launch the browser
     let driver = await new Builder().forBrowser('chrome').build();
 
-    await driver.get('https://to-do.live.com/tasks/myday');
+    //navigate to our applications
+    await driver.get('https://mrsubhonbek.github.io/seleniumTestTypeScript/');
 
-    await driver.findElement(By.id('actionButton')).sendKeys(Key.RETURN)
-    await driver.findElement(By.id('i0116')).sendKeys('Asadulloev_S@outlook.com', Key.RETURN)
+    //add a todo
+    await driver.findElement(By.id('inputId')).sendKeys('Asadulloev_S@outlook.com', Key.RETURN);
+
+    //assert
+    let todoText = await driver.findElement(By.xpath('//p[text() = "Asadulloev_S@outlook.com"]')).getText().then(function (value) {
+        return value;
+    });
+    assert.strictEqual(todoText,'Asadulloev_S@outlook.com');
+    //close the browser
+    await driver.quit();
 }
 example();
